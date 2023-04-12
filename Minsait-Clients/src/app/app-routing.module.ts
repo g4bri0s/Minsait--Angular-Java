75dddc03ea648aa4355d4564f1ab1dd6c343a9b3
g4bri0s <g.cristianods@gmail.com>
2023-04-12 13:21:10 -0300

    Final commit for Angular

--- Minsait-Clients/src/app/app-routing.module.ts
+++ Minsait-Clients/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { EditClientComponent } from './pages/edit-client/edit-client.component';
 import { RegisterClientComponent } from './pages/register-client/register-client.component';
 import { ClientsComponent } from './pages/clients/clients.component';
